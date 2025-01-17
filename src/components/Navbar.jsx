4dc3e8667ecd3f30f06bcae1b3bdd5c5c5cf651f
MukeshMW1 <MukeshBhattaMPB111@gmail.com>
2025-01-17 18:39:45 +0545

    font solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button'
 import { TiLocationArrow } from 'react-icons/ti'
-
+import {useWindowScroll} from 'react-use';
 const navItems =['Nexus','Vault','Prologue','About','Contact']
 
+
+
+
 const Navbar = () => {
+  const [lastScrollY,setLastScrollY] =useState(0);
+  const [isNavVisible,setIsNavVisible] = useState(true)
+
+  const  {y:currentScrollY}  = useWindowScroll()
+
+useEffect(()=>{
+if(currentScrollY === 0)
+{
+  setIsNavVisible(true);
+  navContainerRef.current.classList.remove('floating=nav')
+}
+else if(currentScrollY >lastScrollY){}
+
+
+},[currentScrollY])
+  const handleScroll=()=>{
+    const {y} =useWindowScroll();
+
+  }
 
 const [isAudioPlaying,setIsAudioPlaying]= useState(false)
 const [isIndicatorActive,setIsIndicatorActive]= useState(false)
@@ -32,7 +54,7 @@ else{
 
 
   return (
-    <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 sm:inset-x-6 '>
+    <div ref={navContainerRef}  className='fixed inset-x-0 top-4 z-50 h-16 sm:inset-x-6 '>
       <header className='absolute top-1/2 w-full -translate-y-1/2'>
 
 
