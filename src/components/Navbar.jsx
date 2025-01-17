dfa0637ed979d9471d34d36f690694d248e0a2b1
MukeshMW1 <MukeshBhattaMPB111@gmail.com>
2025-01-17 21:07:28 +0545

    animated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button'
 import { TiLocationArrow } from 'react-icons/ti'
 import {useWindowScroll} from 'react-use';
+import gsap from 'gsap';
 const navItems =['Nexus','Vault','Prologue','About','Contact']
 
 
@@ -12,17 +13,41 @@ const Navbar = () => {
   const [isNavVisible,setIsNavVisible] = useState(true)
 
   const  {y:currentScrollY}  = useWindowScroll()
+  console.log("This is current scroll y",currentScrollY)
 
 useEffect(()=>{
+  console.log("This is last scroll y",lastScrollY)
 if(currentScrollY === 0)
 {
   setIsNavVisible(true);
-  navContainerRef.current.classList.remove('floating=nav')
+  navContainerRef.current.classList.remove('floating-nav')
 }
-else if(currentScrollY >lastScrollY){}
+else if(currentScrollY >lastScrollY){
+  setIsNavVisible(false);
+  navContainerRef.current.classList.remove('floating-nav')
+}
+else if(currentScrollY < lastScrollY){
+  setIsNavVisible(true);
+  navContainerRef.current.classList.add('floating-nav')
+}
+setLastScrollY(currentScrollY)
 
 
-},[currentScrollY])
+},[currentScrollY,lastScrollY])
+
+
+useEffect(()=>{
+
+gsap.to(navContainerRef.current,{
+  y:isNavVisible ? 0 :-100,
+  opacity:isNavVisible ? 1:0,
+  duration:0.5
+  
+})
+
+},[isNavVisible])
+
+
   const handleScroll=()=>{
     const {y} =useWindowScroll();
 
@@ -54,7 +79,7 @@ else{
 
 
   return (
-    <div ref={navContainerRef}  className='fixed inset-x-0 top-4 z-50 h-16 sm:inset-x-6 '>
+    <div ref={navContainerRef}  className={`fixed inset-x-0 top-4 z-50 h-16 sm:inset-x-6 `}>
       <header className='absolute top-1/2 w-full -translate-y-1/2'>
 
 
