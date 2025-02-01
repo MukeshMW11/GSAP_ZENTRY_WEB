76841c75b01c35474b59260a7b330b209d477c88
MukeshMW1 <MukeshBhattaMPB111@gmail.com>
2025-02-01 17:20:15 +0545

    story completed

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import AnimatedTitle from './AnimatedTitle'
 import gsap from 'gsap';
 import RoundedCorners from './RoundedCorners';
+import Button from './Button';
 
 const Story = () => {
 
@@ -61,6 +62,15 @@ gsap.to(element,{
 </div>
 
         </div>
+
+<div className="-mt-52 flex w-full justify-center md:-mt-44 md:me-44 md:justify-end mb-32">
+  <div className="flex h-full w-fit flex-col items-center md:items-start">
+
+<p className='mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start'>
+Where realms converge, lies Zentry and the boundless pillar. Discover its secrets and shape your fate amidst infinite opportunities.</p>
+<Button id='realm-button' title='discover prologue' containerClass='mt-5 text-sm bg-white'/>
+</div>
+  </div>
     </div>
     
     </section>
